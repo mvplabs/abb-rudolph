5b4eced3f0bcf99b0c3481571b000d8ae8ac59ea
Martin Van Aken <martin@joyouscoding.com>
2021-12-21 12:17:46 +0100

    urls

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/abb-rudolph">
         <App />
       </BrowserRouter>
   </React.StrictMode>,
